8139884391a67974f8bfd4300aebaddb51cea1ea
Sohel001-1 <mdnaimuddin2003@gmail.com>
2026-01-29 23:26:25 +0600

    sohel_commit_1_29_26

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,32 +1,22 @@
 import { createContext } from "react";
+import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-  const value = {
-    products: [
-      {
-        _id: '1',
-        name: 'Sample Product 1',
-        image: ['https://via.placeholder.com/150'],
-        price: 100,
-        description: 'A sample product'
-      },
-      {
-        _id: '2',
-        name: 'Sample Product 2',
-        image: ['https://via.placeholder.com/150'],
-        price: 200,
-        description: 'Another sample product'
-      }
-    ]
-  };
 
-  return (
-    <ShopContext.Provider value={value}>
-      {props.children}
-    </ShopContext.Provider>
-  );
-};
+    const currency = '$';
+    const delivery_fee = 10;
+
+    const value = {
+        products , currency, delivery_fee
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
 
 export default ShopContextProvider;
