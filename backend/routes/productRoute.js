61c5c645a995eb2bc39a04bbb5322f3e6c7bc211
Sifat Hakim <gaiejati1@gmail.com>
2026-02-03 13:36:10 +0600

    6:55:53

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import {addProduct,listProduct,removeProduct,singleProduct} from '../controllers/productController.js';
-import upload from '../middlewares/multer.js';
+import upload from '../middleware/multer.js';
 
 const productRouter= express.Router();
 
 //route for adding product
-productRouter.post('/add-product',upload.field([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
+productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
 
 //route for listing products
 productRouter.get('/list-products',listProduct);
