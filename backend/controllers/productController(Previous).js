775803774b0ab7917a41c8a0a0d2d280ca6b8259
Sohel001-1 <mdnaimuddin2003@gmail.com>
2026-02-06 01:59:56 +0600

    till 09.02 skipped 07:08:44 Create authentication for Admin

--- backend/controllers/productController(Previous).js
+++ backend/controllers/productController(Previous).js
@@ -7,7 +7,7 @@ const addProduct =async (req, res) => {
 
     try{
 
-        const {name,description,category,price,subcategory,sizes,bestseller}= req.body;
+        const {name,description,category,price,subCategory,sizes,bestseller}= req.body;
 
         const image1=req.files.image1 &&req.files.image1[0]
         const image2=req.files.image2 &&req.files.image2[0]
