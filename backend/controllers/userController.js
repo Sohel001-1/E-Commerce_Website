a5b7649afdd1a5e0bae925fff823bca59fe2f166
Sifat Hakim <gaiejati1@gmail.com>
2026-02-06 01:57:59 +0600

    admin auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -84,10 +84,25 @@ res.json({success:true,token})
 
 //route for admin login
 const adminLogin=async(req,res)=>{
+    try{
+        const{email,password}=req.body;
+        if(email===process.env.ADMIN_EMAIL && password===process.env.ADMIN_PASSWORD){
+            const token= jwt.sign(email+password,process.env.JWT_SECRET);
+            res.json({success:true,token})
+
+        }else {
+            res.json({success:false,message:"Invalid admin credentials"})
+        }
+
+
+
+}catch (error) {
+    console.log(error);
+    res.json({success:false,message:error.message })
 
 
 }
 
-
+}
 
 export {loginUser,registerUser,adminLogin};
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,19 +1,20 @@
 import express from 'express';
 import {addProduct,listProduct,removeProduct,singleProduct} from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter= express.Router();
 
 //route for adding product
-productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
+productRouter.post('/add',adminAuth,upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
 
 //route for listing products
-productRouter.get('/list-products',listProduct);
+productRouter.get('/list',listProduct);
 
 //route for removing product
-productRouter.delete('/remove-product/:id',removeProduct);
+productRouter.delete('/remove',adminAuth,removeProduct);
 
 //route for single product info
-productRouter.get('/single-product/:id',singleProduct);
+productRouter.get('/single',singleProduct);
 
 export default productRouter;
