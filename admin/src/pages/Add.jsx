e56ccf12884066fa611af55b225a44a9ab67db9c
Sohel Naimuddin-ASUS <mdnaimuddin2003@gmail.com>
2026-02-09 00:49:02 +0600

    sohel282

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -135,11 +135,10 @@ const Add = ({ token }) => {
             <option value="Autodetailing">Autodetailing</option>
             <option value="Engine Oil">Engine Oil</option>
             <option value="Filters">Filters</option>
-            <option value="Brakes">Brakes</option>
             <option value="Damping">Damping</option>
             <option value="Ignition">Ignition</option>
             <option value="Engine">Engine</option>
-           <option value="Brakes">Brakes</option>
+           <option value="Brake">Brake</option>
            <option value="Suspension">Suspension</option>
            <option value="Electrical">Electrical</option>
            <option value="Body">Body</option>
