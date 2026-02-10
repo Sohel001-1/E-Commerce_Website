0755f7953d73cf195eb6444da04897ab4b4fd05f
Sohel Naimuddin-ASUS <mdnaimuddin2003@gmail.com>
2026-02-10 18:35:02 +0600

    profile page

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,45 @@
-import express from 'express';
-import { registerUser, loginUser, adminLogin } from '../controllers/userController.js';
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  adminLogin,
+  getUserProfile,
+  updateUserProfile,
+  addAddress,
+  deleteAddress,
+  setDefaultAddress,
+  uploadProfileImage,
+  toggleWishlist,
+  getWishlist,
+  changePassword,
+} from "../controllers/userController.js";
+import authUser from "../middleware/auth.js";
 
-const userRouter =express.Router();
+const userRouter = express.Router();
 
-userRouter.post('/register', registerUser);
-userRouter.post('/login', loginUser);
-userRouter.post('/admin/login', adminLogin);
-userRouter.post('/admin', adminLogin);
+// Public Routes
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/admin", adminLogin);
 
+// Protected Profile Routes
+// Note: authUser middleware extracts the userId from the token
+userRouter.get("/get-profile", authUser, getUserProfile);
+userRouter.post("/update-profile", authUser, updateUserProfile);
+
+// Address Management Routes
+userRouter.post("/add-address", authUser, addAddress);
+userRouter.post("/address/delete", authUser, deleteAddress);
+userRouter.post("/address/default", authUser, setDefaultAddress);
+
+// Image Upload Route
+userRouter.post("/upload-image", authUser, uploadProfileImage);
+
+// Wishlist Routes
+userRouter.post("/toggle-wishlist", authUser, toggleWishlist);
+userRouter.get("/wishlist", authUser, getWishlist);
+
+// Password Change Route
+userRouter.post("/change-password", authUser, changePassword);
 
 export default userRouter;
