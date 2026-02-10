a03b904e9b7a9127337bd82e98d15b24d4da815a
Sohel Naimuddin-ASUS <mdnaimuddin2003@gmail.com>
2026-02-10 22:20:10 +0600

    home-page category dark

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -4,7 +4,7 @@ const Title = ({ text1, text2 }) => {
   return (
     <div className='inline-flex gap-2 items-center mb-3'>
       <p className='text-surface-400 font-display'>
-        {text1}{' '}
+        {text1}{''}
         <span className='font-bold text-surface-800'>{text2}</span>
       </p>
       <p className='w-8 sm:w-12 h-[2px] bg-gradient-to-r from-brand-500 to-orange-300 rounded-full'></p>
