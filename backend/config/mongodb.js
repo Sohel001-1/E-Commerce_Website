443d7b4ff827bec34e1b88e73b43e64eaf07b49b
Sohel Naimuddin-ASUS <mdnaimuddin2003@gmail.com>
2026-02-20 23:45:43 +0600

    connectionerror

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+// Fix for Node.js DNS resolution issues with MongoDB SRV records on some local networks
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
   try {
